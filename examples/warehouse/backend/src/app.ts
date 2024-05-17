55d81c654f169c519eb065c55fde840a1d7b7b7d
eliav2 <louski.a@gmail.com>
2024-05-17 23:40:43 +0300

    dev

--- examples/warehouse/backend/src/app.ts
+++ examples/warehouse/backend/src/app.ts
@@ -1,80 +1,112 @@
-import express, { Request, Response } from "express";
-import logger from "morgan";
-import "dotenv/config";
-import { createRequire } from "module";
-import * as process from "process";
-import cookieSession from "cookie-session";
-import { getPrisma } from "./getPrisma.js";
-import { errorHandler } from "./middleware/error.middleware.js";
-import { cookieSessionFixMiddleware } from "./middleware/cookieSessionFix.middleware.js";
-import authRouter from "./routes/auth.routes.js";
+import logger from 'morgan';
+import 'dotenv/config';
+import { createRequire } from 'module';
+import * as process from 'process';
+import cookieSession from 'cookie-session';
+import { getPrisma } from './getPrisma.js';
+import { errorHandler } from './middleware/error.middleware.js';
+import { cookieSessionFixMiddleware } from './middleware/cookieSessionFix.middleware.js';
+import express, { Request, Response } from 'express';
+import expressWS from 'express-ws';
+
+import authRouter from './routes/auth.routes.js';
 
 const require = createRequire(import.meta.url);
-const { ZenStackMiddleware } = require("@zenstackhq/server/express");
+const { ZenStackMiddleware } = require('@zenstackhq/server/express');
 
-if (!process.env.AUTH_SECRET)
-  throw new Error("AUTH_SECRET env variable is required");
+if (!process.env.AUTH_SECRET) throw new Error('AUTH_SECRET env variable is required');
 
 // Create Express server
 export const app = express();
+const wsApp = expressWS(app);
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 // Trust Proxy for Proxies (Heroku, Render.com, etc)
 // https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa
-app.enable("trust proxy");
+app.enable('trust proxy');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 // Parse incoming requests data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(
-  cookieSession({
-    name: "session",
-    keys: [process.env.AUTH_SECRET],
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-  }),
+    cookieSession({
+        name: 'session',
+        keys: [process.env.AUTH_SECRET],
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    }),
 );
 
 app.use(cookieSessionFixMiddleware);
 
-app.use("/auth", authRouter);
+app.use('/auth', authRouter);
 
 app.use(
-  "/model",
-  ZenStackMiddleware({
-    getPrisma: getPrisma,
-    zodSchemas: true,
-  }),
+    '/model',
+    ZenStackMiddleware({
+        getPrisma: getPrisma,
+        zodSchemas: true,
+    }),
 );
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send("backend up").status(200);
+app.get('/', async (req: Request, res: Response) => {
+    res.send('backend up').status(200);
 });
 
-import WebSocket, { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 8081 });
-
-wss.on("connection", (ws) => {
-  setTimeout(() => {
-    ws.send("Hey");
-    ws.close();
-  }, 5000);
+app!.ws('/test-transaction', function (ws, req) {
+    console.log('this would run');
+    ws.on('message', function (msg) {
+        ws.send(msg);
+    });
+    // ws.close();
 });
 
-app.get("/test-transaction", async (req: Request, res: Response) => {
-  const ws = new WebSocket("ws://localhost:8081");
+import http from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
 
-  ws.on("open", function open() {
-    ws.send("something");
-  });
+const wss = new WebSocketServer({ noServer: true });
+export const appServer = http.createServer(app);
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        ws.send('Message received: ' + message);
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket connection closed');
+    });
+
+    ws.on('error', (err) => {
+        console.error('WebSocket error:', err);
+    });
 });
-app.get("/error", (req, res) => {
-  throw new Error("Error route");
+// appServer.on('upgrade', (request, socket, head) => {
+//     if (request.url === '/test-transaction') {
+//         wss.handleUpgrade(request, socket, head, (ws) => {
+//             wss.emit('connection', ws, request);
+//         });
+//     } else {
+//         socket.destroy();
+//     }
+// });
+
+// app.get('/test-transaction', async (req: Request, res: Response) => {
+//     console.log('test-transaction');
+//     const ws = new WebSocket('ws://localhost:8081');
+//
+//     ws.on('open', function open() {
+//         console.log('sending');
+//         ws.send('something');
+//     });
+// });
+
+app.get('/error', (req, res) => {
+    throw new Error('Error route');
 });
 
 // Error handlers
--- examples/warehouse/frontend/src/routes/index.tsx
+++ examples/warehouse/frontend/src/routes/index.tsx
@@ -1,27 +1,199 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Box, Divider, Typography } from "@mui/material";
-import Button from "@mui/material/Button";
-import Link from "../components/Link.tsx";
+import { createFileRoute } from '@tanstack/react-router';
+import { Box, Divider, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Link from '../components/Link.tsx';
+import { useEffect, useRef, useState } from 'react';
 
-export const Route = createFileRoute("/")({
-  component: HomeComponent,
+export const Route = createFileRoute('/')({
+    component: HomeComponent,
 });
 
-function HomeComponent() {
-  // return <App />;
-  return (
-    <Box sx={{ display: "flex", flexDirection: "column", p: 2 }}>
-      <Typography variant="h2">Welcome To zenstack demo</Typography>
-      <Divider />
-      <Typography sx={{ fontSize: "1.3em", p: 3 }}>
-        backend - zenstack+passport+github sso
-        <br />
-        frontend - react + mui5 + tanstack query + tanstack router - recoil
-      </Typography>
-
-      <Link to={"/products"} component={Box} sx={{ alignSelf: "center" }}>
-        <Button>Let's get started →</Button>
-      </Link>
-    </Box>
-  );
+interface TransactionProxy {
+    create: () => Promise<void>;
+    update: () => Promise<void>;
 }
+
+function waitForResponse(socket: WebSocket): Promise<MessageEvent> {
+    return new Promise((resolve) => {
+        socket.onmessage = (event) => {
+            resolve(event);
+        };
+    });
+}
+
+async function sendAndAwaitResponse(socket: WebSocket, message: string): Promise<MessageEvent> {
+    // Send the message
+    socket.send(message);
+    // Wait for the response
+    const response = await waitForResponse(socket);
+    return response;
+}
+
+const sendTransaction = () => {
+    // const [transactionState, setTransactionState] = useState<'IDLE' | 'CONNECTING' | 'SENDING' | 'WAITING'>('IDLE');
+    // const socketRef = useRef<WebSocket | null>(null);
+    // const transactionHandlerRef = useRef<((tx: TransactionProxy) => Promise<void>) | null>(null);
+
+    // useEffect(() => {
+    //     if (transactionState === 'CONNECTING') {
+    //         let socket = new WebSocket('ws://localhost:3000/test-transaction');
+    //         // Connection opened
+    //         socket.addEventListener('open', function (event) {
+    //             console.log('Connected to server');
+    //             setTransactionState('SENDING');
+    //             socketRef.current = socket;
+    //         });
+    //         // Connection closed
+    //         socket.addEventListener('close', function (event) {
+    //             console.log('Server connection closed: ', event.code);
+    //         });
+    //         // Connection error
+    //         socket.addEventListener('error', function (event) {
+    //             console.log('WebSocket error: ', event);
+    //         });
+    //         // Listen for messages
+    //         socket.addEventListener('message', function (event) {
+    //             console.log('Message from server: ', event.data);
+    //         });
+    //     } else if (transactionState === 'SENDING') {
+    //         transactionHandlerRef
+    //             .current?.({
+    //                 create: async () => {
+    //                     // socketRef.current?.send('Create');
+    //                     const messageEvent = await sendAndAwaitResponse(socketRef.current, 'Create');
+    //                     console.log('Create response:', messageEvent.data);
+    //                 },
+    //                 update: async () => {
+    //                     // socketRef.current?.send('Update');
+    //                     const messageEvent = await sendAndAwaitResponse(socketRef.current, 'Update');
+    //                 },
+    //             })
+    //             ?.then(() => {
+    //                 socketRef.current?.close();
+    //                 console.log('closing!');
+    //                 setTransactionState('IDLE');
+    //                 socketRef.current = null;
+    //                 transactionHandlerRef.current = null;
+    //             });
+    //     }
+    // }, [transactionState]);
+
+    return (transactionHandler: (tx: TransactionProxy) => Promise<void>) => {
+        const socket = new WebSocket('ws://localhost:3000/test-transaction');
+        socket.onopen = async (event) => {
+            console.log('Connected to server');
+
+            await transactionHandler({
+                create: async () => {
+                    const messageEvent = await sendAndAwaitResponse(socket, 'Create');
+                    console.log('Create response:', messageEvent.data);
+                },
+                update: async () => {
+                    const messageEvent = await sendAndAwaitResponse(socket, 'Update');
+                    console.log('Update response:', messageEvent.data);
+                },
+            });
+            socket.close();
+            console.log('closing!');
+
+            // resolve((transactionHandler) => {
+            //     // transactionHandlerRef.current = transactionHandler;
+            //     // socketRef.current?.send('Hello Server 123!');
+            // });
+
+            // // Send the message
+            // socket.send(message);
+            //
+            // // Wait for the response
+            // socket.onmessage = (event) => {
+            //     resolve(event);
+            // };
+        };
+    };
+};
+
+function HomeComponent() {
+    const transaction = sendTransaction();
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', p: 2 }}>
+            <Button
+                onClick={() => {
+                    transaction(async (tx) => {
+                        await tx.create();
+                        await tx.update();
+                    });
+                }}
+            >
+                Send
+            </Button>
+            <Typography variant="h2">Welcome To zenstack demo</Typography>
+            <Divider />
+            <Typography sx={{ fontSize: '1.3em', p: 3 }}>
+                backend - zenstack+passport+github sso
+                <br />
+                frontend - react + mui5 + tanstack query + tanstack router - recoil
+            </Typography>
+
+            <Link to={'/products'} component={Box} sx={{ alignSelf: 'center' }}>
+                <Button>Let's get started →</Button>
+            </Link>
+        </Box>
+    );
+}
+
+// const sendTransaction = () => {
+//     const [transactionState, setTransactionState] = useState<'IDLE' | 'CONNECTING' | 'SENDING' | 'WAITING'>('IDLE');
+//     const socketRef = useRef<WebSocket | null>(null);
+//     const transactionHandlerRef = useRef<((tx: TransactionProxy) => void) | null>(null);
+//     const [ongoingTransactions, setOngoingTransactions] = useState<string[]>([]);
+//     useEffect(() => {
+//         if (transactionState === 'CONNECTING') {
+//             let socket = new WebSocket('ws://localhost:3000/test-transaction');
+//             // Connection opened
+//             socket.addEventListener('open', function (event) {
+//                 console.log('Connected to server');
+//                 setTransactionState('SENDING');
+//                 socketRef.current = socket;
+//                 // socket.close();
+//             });
+//             // Connection closed
+//             socket.addEventListener('close', function (event) {
+//                 console.log('Server connection closed: ', event.code);
+//             });
+//             // Connection error
+//             socket.addEventListener('error', function (event) {
+//                 console.log('WebSocket error: ', event);
+//             });
+//             // Listen for messages
+//             socket.addEventListener('message', function (event) {
+//                 console.log('Message from server: ', event.data);
+//             });
+//         } else if (transactionState === 'SENDING') {
+//             transactionHandlerRef.current?.({
+//                 create: async () => {
+//                     socketRef.current?.send('Create');
+//                     setOngoingTransactions((prev) => [...prev, 'Create']);
+//                 },
+//                 update: async () => {
+//                     socketRef.current?.send('Update');
+//                     setOngoingTransactions((prev) => [...prev, 'Update']);
+//                 },
+//             });
+//             setTransactionState('WAITING');
+//         } else if (transactionState === 'WAITING') {
+//             if (ongoingTransactions.length === 0) {
+//                 socketRef.current?.close();
+//                 setTransactionState('IDLE');
+//                 socketRef.current = null;
+//                 transactionHandlerRef.current = null;
+//             }
+//         }
+//     }, [transactionState]);
+//
+//     return (transactionHandler: (tx: TransactionProxy) => Promise<void>) => {
+//         // socketRef.current?.send('Hello Server 123!');
+//
+//         setTransactionState('CONNECTING');
+//         transactionHandlerRef.current = transactionHandler;
+//     };
+// };
