fb08b3c768c0680e63baeb564e30dd4b1f111ea0
eliav2 <louski.a@gmail.com>
2024-05-17 23:40:43 +0300

    dev

--- examples/warehouse/backend/src/server.ts
+++ examples/warehouse/backend/src/server.ts
@@ -1,15 +1,18 @@
-import "./globals.js";
-import * as dotenv from "dotenv";
+import './globals.js';
+import * as dotenv from 'dotenv';
+// import { appServer } from './app.js';
 // Load environment variables
-dotenv.config({ path: __dirname + "/../.env" });
+dotenv.config({ path: __dirname + '/../.env' });
 
 // Import app after environment variables are set
-const { app } = await import("./app.js");
+const { app } = await import('./app.js');
 
-const port = app.get("port");
+const port = app.get('port');
 
 const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
-
+// appServer.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+// });
 export default server;
